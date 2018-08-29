fc554988cd5d7c9fcfa8a2b9d3eb3fdaec7becdf
petercombs <pcombs@stanford.edu>
2018-08-29 11:46:52 -0700

    Plot top individual snps
    This changes the output format of ScoreSnps to include the SNPs
    alternative allele frequencies (when available, otherwise if no coverage
    defaults to 0).

--- CombinePvals.py
+++ CombinePvals.py
@@ -9,40 +9,65 @@ drawn from a uniform distribution.
 
 """
 import pandas as pd
+from os import path
 from numpy import arange, log10
 from argparse import ArgumentParser
 from scipy.stats import combine_pvalues
+from matplotlib.pyplot import (
+    xlim,
+    ylim,
+    plot,
+    scatter,
+    xlabel,
+    ylabel,
+    legend,
+    savefig,
+    figure,
+    subplot,
+    close,
+)
+from numpy.random import shuffle
 from tqdm import tqdm
-from matplotlib.pyplot import plot, scatter, xlabel, ylabel, savefig
 
 
 def parse_args():
     "Program specific argument parsing"
     parser = ArgumentParser()
     parser.add_argument("--output-prefix", "-o")
+    parser.add_argument("--num-subplots", type=int, default=16)
     parser.add_argument("scores", nargs="+")
 
-    return parser.parse_args()
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == "__main__":
     args = parse_args()
+    outdir = path.dirname(args.output_prefix)
 
     pvals_to_combine_fwd = {}
     pvals_to_combine_rev = {}
+    pvals_to_combine_rand = {}
+    fet_data = {}
     for file in args.scores:
-        fet_pvals = pd.read_table(file, squeeze=True, header=None, index_col=0)
+        fet_pvals = pd.read_table(file, squeeze=True, index_col=0).sort_values("pval")
         n = len(fet_pvals)
         expected = arange(1, n + 1) / n
         semi_ps = pd.Series(index=fet_pvals.index, data=expected).sort_index()
+        semi_ps_rand = semi_ps.copy()
+        shuffle(semi_ps_rand)
+        fet_data[file] = fet_pvals.sort_index()
         pvals_to_combine_fwd[file] = semi_ps
-        pvals_to_combine_rev[file] = 1 - semi_ps + 1/n
+        pvals_to_combine_rev[file] = 1 - semi_ps + 1 / n
+        pvals_to_combine_rand[file] = semi_ps_rand
 
     pvals_to_combine_fwd = pd.DataFrame(pvals_to_combine_fwd)
     pvals_to_combine_rev = pd.DataFrame(pvals_to_combine_rev)
+    pvals_to_combine_rand = pd.DataFrame(pvals_to_combine_rand)
 
     combined_pvals_fwd = pd.Series(index=pvals_to_combine_fwd.index, data=0.0)
     combined_pvals_rev = pd.Series(index=pvals_to_combine_rev.index, data=0.0)
+    combined_pvals_rand = pd.Series(index=pvals_to_combine_rev.index, data=0.0)
 
     for ix in tqdm(combined_pvals_fwd.index):
         # Multiply by two to correct for testing both ends
@@ -53,21 +78,55 @@ if __name__ == "__main__":
             combine_pvalues(pvals_to_combine_rev.loc[ix], method="fisher") * 2
         )[1]
 
+        combined_pvals_rand[ix] = (
+            combine_pvalues(pvals_to_combine_rand.loc[ix], method="fisher") * 2
+        )[1]
+
+    combined_pvals_fwd.sort_values(inplace=True)
+    combined_pvals_rev.sort_values(inplace=True)
+    combined_pvals_rand.sort_values(inplace=True)
+
     combined_pvals_fwd.to_csv(args.output_prefix + ".Stalk.tsv", sep="\t")
     combined_pvals_rev.to_csv(args.output_prefix + ".Spore.tsv", sep="\t")
 
+    figure()
     scatter(
-        -log10(expected),
-        -log10(combined_pvals_fwd.sort_values()),
-        label="Stalk? specific",
+        -log10(combined_pvals_rand), -log10(combined_pvals_fwd), label="Stalk? specific"
     )
     scatter(
-        -log10(expected),
-        -log10(combined_pvals_rev.sort_values()),
-        label="Spore? specific",
+        -log10(combined_pvals_rand), -log10(combined_pvals_rev), label="Spore? specific"
     )
 
     plot([0, 17], [0, 17], "r:")
     xlabel("Expected")
     ylabel("Observed")
-    savefig("analysis/results/combined_pvals_fwd_and_rev.png")
+    legend(loc="lower right")
+    savefig(path.join(outdir, "combined_pvals_fwd_and_rev.png"))
+    close()
+
+    n_rows = int(pd.np.ceil(pd.np.sqrt(args.num_subplots)))
+    n_cols = args.num_subplots // n_rows
+    assert n_rows * n_cols >= args.num_subplots
+
+    for name, dataset in (
+        ("stalk", combined_pvals_fwd),
+        ("spore", combined_pvals_rev),
+        ("random", combined_pvals_rand),
+    ):
+        figure()
+
+        for i in range(args.num_subplots):
+            snp = dataset.index[i]
+            subplot(n_rows, n_cols, i + 1)
+            stalks = [fet_data[file].ix[snp, "stalk"] for file in args.scores]
+            spores = [fet_data[file].ix[snp, "spore"] for file in args.scores]
+            scatter(stalks, spores)
+            xlim(0, 1)
+            ylim(0, 1)
+            if i % n_cols == 0:
+                ylabel("Spores")
+            if i // n_cols == n_rows - 1:
+                xlabel("Stalks")
+
+        savefig(path.join(outdir, "{}_snps.png".format(name)))
+        close()
--- ScoreSnps.py
+++ ScoreSnps.py
@@ -1,3 +1,12 @@
+""" Come up with a ranking of SNPs within each fruiting body
+
+Use Fishers Exact Test to come up with a ranking of SNPs within each Stalk/Spore
+pair.
+
+I am not putting too much stock in these p-values as meaningful, so later
+steps in the pipeline will effectively just use the rank order.
+
+"""
 import pandas as pd
 from argparse import ArgumentParser
 from tqdm import tqdm
@@ -13,45 +22,60 @@ def pipesplit(col):
 
 base_cols = {"A": 0, "C": 1, "G": 2, "T": 3}
 
+
 def read_snpcounts(fname):
     outdict = {}
     with open(fname) as fh:
-        next(fh) # Skip header
+        next(fh)  # Skip header
         for line in fh:
             chr, pos, ref, alt, nonra = line.split()
             outdict[(chr, int(pos))] = [int(ref), int(alt)]
     return outdict
 
+
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("snp_count_1")
-    parser.add_argument("snp_count_2")
+    parser.add_argument("stalk_count")
+    parser.add_argument("spore_count")
     parser.add_argument("output")
 
     args = parser.parse_args()
-    args.snp_count_1 = read_snpcounts(args.snp_count_1)
-    args.snp_count_2 = read_snpcounts(args.snp_count_2)
+    args.stalk_count = read_snpcounts(args.stalk_count)
+    args.spore_count = read_snpcounts(args.spore_count)
     return args
 
 
 if __name__ == "__main__":
     args = parse_args()
 
-    in_both = set(args.snp_count_1).intersection(args.snp_count_2)
-    out = {}
+    in_both = set(args.stalk_count).intersection(args.spore_count)
+    out_pvals = {}
+    out_stalk_altratio = {}
+    out_spore_altratio = {}
     pool = Pool()
     for pos in tqdm(in_both):
         table = [
-            [args.snp_count_1[pos][0], args.snp_count_1[pos][1]],
-            [args.snp_count_2[pos][0], args.snp_count_2[pos][1]],
+            [args.stalk_count[pos][0], args.stalk_count[pos][1]],
+            [args.spore_count[pos][0], args.spore_count[pos][1]],
         ]
-        out["{}:{:07d}".format(*pos)] = pool.apply_async(fisher_exact, [table])
+        snpid = "{}:{:07d}".format(*pos)
+        out_pvals[snpid] = pool.apply_async(fisher_exact, [table])
+        out_stalk_altratio[snpid] = args.stalk_count[pos][1] / (
+            (args.stalk_count[pos][0] + args.stalk_count[pos][1]) or 1
+        )
+        out_spore_altratio[snpid] = args.spore_count[pos][1] / (
+            (args.spore_count[pos][0] + args.spore_count[pos][1]) or 1
+        )
 
-    for id in tqdm(out):
-        odds_r, pval = out[id].get()
+    for id in tqdm(out_pvals):
+        odds_r, pval = out_pvals[id].get()
         if odds_r > 0:
-            out[id] = pval / 2
+            out_pvals[id] = pval / 2
         else:
-            out[id] = 1 - pval / 2
-    out = pd.Series(out)
-    out.sort_values().to_csv(args.output, sep="\t")
+            out_pvals[id] = 1 - pval / 2
+    out = pd.DataFrame(
+        {"pval": out_pvals, "stalk": out_stalk_altratio, "spore": out_spore_altratio}
+    )
+
+    out.index.name = "snp_id"
+    out.sort_values(by="pval").to_csv(args.output, sep="\t")
