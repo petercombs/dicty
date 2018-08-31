afbb76e4218544b9e8c037e148659cdab2dece2b
petercombs <pcombs@stanford.edu>
2018-08-31 13:13:05 -0700

    Refactor loading data to a new function and try mp fisher
    The fisher seems to be limited by reading from the dataframe, rather
    than actually doing the calculations. So as it's currently written, I
    don't get much speedup...

--- CombinePvals.py
+++ CombinePvals.py
@@ -9,6 +9,7 @@ drawn from a uniform distribution.
 
 """
 import pandas as pd
+import numpy as np
 from os import path
 from numpy import arange, log10, nan, ceil, sqrt, isfinite
 from argparse import ArgumentParser
@@ -54,7 +55,62 @@ def parse_args():
     return args
 
 
-def make_qq_plot(combine_pvals_fwd, combined_pvals_rev, combined_pvals_rand):
+def load_data(filenames):
+    "Load SNP scores"
+    # To-do: make the returns more organized.
+    pvals_to_combine_fwd = {}
+    pvals_to_combine_rev = {}
+    pvals_to_combine_rand = {}
+
+    fet_data = {}
+
+    all_stalk_freqs = []
+    all_spore_freqs = []
+
+    for file in tqdm(filenames):
+        fet_file = pd.read_table(file, squeeze=True, index_col=0)
+        fet_pvals = fet_file.pval.copy()
+        good_snps = isfinite(fet_file["rank"])
+        great_snps = (fet_file.iloc[:, 1:3].T.sum() > 10) & (
+            fet_file.iloc[:, 3:5].T.sum() > 10
+        )
+        fet_pvals[good_snps] = nan
+        if "all_good_snps" not in locals():
+            all_good_snps = good_snps.copy()
+        all_good_snps |= good_snps
+
+        all_stalk_freqs.extend(fet_file.loc[great_snps, "stalk_ratio"])
+        all_spore_freqs.extend(fet_file.loc[great_snps, "spore_ratio"])
+
+        semi_ps = fet_file["rank"] / fet_file.maxrank
+
+        # Shuffling a Series is apparently wicked slow
+        semi_ps_rand_to_shuffle = np.array(semi_ps[good_snps])
+        shuffle(semi_ps_rand_to_shuffle)
+        semi_ps_rand = pd.Series(index=semi_ps.index, data=np.nan)
+        semi_ps_rand[good_snps] = semi_ps_rand_to_shuffle
+
+        fet_data[file] = fet_file.sort_index()
+        pvals_to_combine_fwd[file] = semi_ps
+        pvals_to_combine_rev[file] = 1 - semi_ps + 1 / fet_file["rank"].max()
+        pvals_to_combine_rand[file] = semi_ps_rand
+
+    pvals_to_combine_fwd = pd.DataFrame(pvals_to_combine_fwd, dtype=float)
+    pvals_to_combine_rev = pd.DataFrame(pvals_to_combine_rev, dtype=float)
+    pvals_to_combine_rand = pd.DataFrame(pvals_to_combine_rand, dtype=float)
+
+    return (
+        pvals_to_combine_fwd,
+        pvals_to_combine_rev,
+        pvals_to_combine_rand,
+        all_stalk_freqs,
+        all_spore_freqs,
+        all_good_snps,
+        fet_data,
+    )
+
+
+def make_qq_plot(combined_pvals_fwd, combined_pvals_rev, combined_pvals_rand):
     figure()
     scatter(
         -log10(combined_pvals_rand), -log10(combined_pvals_fwd), label="Spore specific"
@@ -155,69 +211,55 @@ if __name__ == "__main__":
     args = parse_args()
     outdir = path.dirname(args.output_prefix)
 
-    pvals_to_combine_fwd = {}
-    pvals_to_combine_rev = {}
-    pvals_to_combine_rand = {}
+    (
+        pvals_to_combine_fwd,
+        pvals_to_combine_rev,
+        pvals_to_combine_rand,
+        all_stalk_freqs,
+        all_spore_freqs,
+        all_good_snps,
+        fet_data,
+    ) = load_data(args.scores)
 
-    fet_data = {}
-
-    all_stalk_freqs = []
-    all_spore_freqs = []
-
-    for file in tqdm(args.scores):
-        fet_file = pd.read_table(file, squeeze=True, index_col=0)
-        fet_pvals = fet_file.pval.copy()
-        good_snps = isfinite(fet_file["rank"])
-        great_snps = (fet_file.iloc[:, 1:3].T.sum() > 10) & (
-            fet_file.iloc[:, 3:5].T.sum() > 10
+    if not args.skip_fisher:
+        combined_pvals_fwd = pd.Series(
+            index=pvals_to_combine_fwd.index, data=nan, dtype=float
         )
-        fet_pvals[good_snps] = nan
-        if "all_good_snps" not in locals():
-            all_good_snps = good_snps.copy()
-        all_good_snps |= good_snps
+        combined_pvals_rev = pd.Series(
+            index=pvals_to_combine_rev.index, data=nan, dtype=float
+        )
+        combined_pvals_rand = pd.Series(
+            index=pvals_to_combine_rev.index, data=nan, dtype=float
+        )
 
-        all_stalk_freqs.extend(fet_file.loc[great_snps, "stalk_ratio"])
-        all_spore_freqs.extend(fet_file.loc[great_snps, "spore_ratio"])
+        fwd_results_queue = {}
+        rev_results_queue = {}
+        rand_results_queue = {}
+        with Pool() as pool:
+            for ix in tqdm(all_good_snps.index[all_good_snps]):
+                fwd_results_queue[ix] = pool.apply_async(
+                    combine_pvalues, [pvals_to_combine_fwd.loc[ix].dropna(), "fisher"]
+                )
+                rev_results_queue[ix] = pool.apply_async(
+                    combine_pvalues, [pvals_to_combine_rev.loc[ix].dropna(), "fisher"]
+                )
+                rand_results_queue[ix] = pool.apply_async(
+                    combine_pvalues, [pvals_to_combine_rand.loc[ix].dropna(), "fisher"]
+                )
 
-        n = len(fet_pvals)
-        fet_pvals_jitter = (fet_pvals + .01 * rand() * fet_pvals.min()).sort_index()
+            for ix in tqdm(all_good_snps.index[all_good_snps]):
+                # Multiply by two to correct for testing both ends
+                combined_pvals_fwd[ix] = fwd_results_queue[ix].get()[1]
+                combined_pvals_rev[ix] = rev_results_queue[ix].get()[1]
+                combined_pvals_rand[ix] = rand_results_queue[ix].get()[1]
 
-        expected = arange(1, n + 1) / fet_pvals.count() * pd.np.sign(fet_pvals)
-        semi_ps = fet_file['rank'] / fet_file.maxrank
-        semi_ps_rand = semi_ps.copy()
-        shuffle(semi_ps_rand)
-        fet_data[file] = fet_file.sort_index()
-        pvals_to_combine_fwd[file] = semi_ps
-        pvals_to_combine_rev[file] = 1 - semi_ps + 1 / fet_file['rank'].max()
-        pvals_to_combine_rand[file] = semi_ps_rand
+        combined_pvals_fwd.sort_values(inplace=True)
+        combined_pvals_rev.sort_values(inplace=True)
+        combined_pvals_rand.sort_values(inplace=True)
 
-    pvals_to_combine_fwd = pd.DataFrame(pvals_to_combine_fwd)
-    pvals_to_combine_rev = pd.DataFrame(pvals_to_combine_rev)
-    pvals_to_combine_rand = pd.DataFrame(pvals_to_combine_rand)
+        combined_pvals_fwd.to_csv(args.output_prefix + ".Stalk.tsv", sep="\t")
+        combined_pvals_rev.to_csv(args.output_prefix + ".Spore.tsv", sep="\t")
 
-    combined_pvals_fwd = pd.Series(index=pvals_to_combine_fwd.index, data=0.0)
-    combined_pvals_rev = pd.Series(index=pvals_to_combine_rev.index, data=0.0)
-    combined_pvals_rand = pd.Series(index=pvals_to_combine_rev.index, data=0.0)
-
-    for ix in tqdm(combined_pvals_fwd.index):
-        # Multiply by two to correct for testing both ends
-        combined_pvals_fwd[ix] = (
-            combine_pvalues(pvals_to_combine_fwd.loc[ix].dropna(), method="fisher") * 2
-        )[1]
-        combined_pvals_rev[ix] = (
-            combine_pvalues(pvals_to_combine_rev.loc[ix].dropna(), method="fisher") * 2
-        )[1]
-
-        combined_pvals_rand[ix] = (
-            combine_pvalues(pvals_to_combine_rand.loc[ix].dropna(), method="fisher") * 2
-        )[1]
-
-    combined_pvals_fwd.sort_values(inplace=True)
-    combined_pvals_rev.sort_values(inplace=True)
-    combined_pvals_rand.sort_values(inplace=True)
-
-    combined_pvals_fwd.to_csv(args.output_prefix + ".Stalk.tsv", sep="\t")
-    combined_pvals_rev.to_csv(args.output_prefix + ".Spore.tsv", sep="\t")
     make_qq_plot(combined_pvals_fwd, combined_pvals_rev, combined_pvals_rand)
 
     make_tehranchigram(all_stalk_freqs, all_spore_freqs)
