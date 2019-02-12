f95ceb3966b45790c731c5c9c3c2a652fc0bc6fe
petercombs <pcombs@stanford.edu>
2019-02-11 17:35:40 -0800

    Wire in the code for plotting by groups of SNPs
    Borrowed from my lab notebook. But it should work, ish.

--- PlotLD.py
+++ PlotLD.py
@@ -1,6 +1,11 @@
 from argparse import ArgumentParser
 import pandas as pd
 import numpy as np
+import itertools as it
+from scipy.stats import spearmanr
+from os.path import basename, splitext, dirname, join
+from matplotlib.pyplot import figure, scatter, plot, savefig, close, xlim
+from collections import defaultdict
 
 
 def make_ld_plot(
@@ -51,22 +56,81 @@ def make_ld_plot(
         figure()
     # corrs = corrs.dropna()
     is_good = counts > 10
-    plot((corrs.index * bin_size)[is_good], corrs[is_good], label="Correlation")
-    xlim(xmin, min(xmax, max_dist))
-    ylim(-1, 1)
-    outfile = path.join(outdir, "{}_ld.png".format(name))
-    mpl.savefig(outfile)
+    # plot((corrs.index * bin_size)[is_good], corrs[is_good], label="Correlation")
+    # xlim(xmin, min(xmax, max_dist))
+    # ylim(-1, 1)
+    # outfile = join(outdir, "{}_ld.png".format(name))
+    # mpl.savefig(outfile)
     # counts.plot()
+    plot_groupbins(pairs_by_dist, name, outdir=outdir)
     return corrs, counts, pairs_by_dist
+
+
+def plot_sizebins():
+    pass
+
+
+def plot_groupbins(pairs_by_dist, name, groupsize=50, outdir="analysis/results/"):
+    latest_bin = []
+    bin_colors = []
+    snp_bins = []
+
+    for k, v in sorted(pairs_by_dist.items()):
+        np.random.shuffle(v)
+        while len(v) + len(latest_bin) > 200:
+            n = 200 - len(latest_bin)
+            latest_bin.extend(v[:n])
+            v = v[n:]
+            snp_bins.append(latest_bin)
+            bin_colors.append(k)
+            latest_bin = []
+        latest_bin.extend(v)
+    snp_bins.append(latest_bin)
+    bin_colors.append(k)
+
+    print(min(bin_colors), max(bin_colors))
+    colorcycle = [
+        "blue",
+        "orange",
+        "green",
+        "red",
+        "purple",
+        "brown",
+        "pink",
+        "gray",
+        "lime",
+        "teal",
+        "blue",
+    ]
+    cnames = {i: c for i, c in zip(range(1, max(bin_colors) + 1), it.cycle(colorcycle))}
+
+    pairs_at_dist = pd.Series(
+        {k: len(pairs_by_dist[k]) for k in pairs_by_dist}
+    ).sort_index()
+    pairs_at_dist.plot.bar()
+    xlim(-1, 50)
+    savefig(join(outdir, "{}_group{:03d}_hist.png".format(name, groupsize)))
+    close()
+    figure()
+    scatter(
+        np.arange(len(snp_bins)),
+        [spearmanr(*zip(*snp_bin)).correlation for snp_bin in snp_bins],
+        c=[cnames[i] for i in bin_colors],
+        s=4,
+    )
+
+    savefig(join(outdir, "{}_group{:03d}.png".format(name, groupsize)))
+    print(join(outdir, "{}_group{:03d}.png".format(name, groupsize)))
+    close()
 
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument("scorefile")
-    parser.add_argument("snp_set", nargs="+")
+    parser.add_argument("snp_set", nargs="+", help="Bed file of SNPs to test")
 
     args = parser.parse_args()
-    args.scorefile = pd.read_table(args.scorefile, index_col=0)
+    args.scores = pd.read_table(args.scorefile, index_col=0)
 
     return args
 
@@ -74,4 +138,13 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     for snp_set in args.snp_set:
-        pass
+        snps = []
+        for line in open(snp_set):
+            data = line.split()
+            snps.append("{}:{:07}".format(data[0], int(data[1]) - 1))
+
+        make_ld_plot(
+            args.scores.loc[snps, "spore"].dropna(),
+            name=splitext(basename(snp_set))[0],
+            outdir=dirname(args.scorefile),
+        )
