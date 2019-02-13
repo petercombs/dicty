c90230d1770c8c56610148b50a11cda9ebdd40d3
petercombs <pcombs@stanford.edu>
2019-02-13 10:32:19 -0800

    Remove vestigial LD Plot code from CombinePvals
    The code has been moved (and expanded) in PlotLD.py. Especially as I
    spin out more code to separate plotting functions, having this inside
    CombinePvals is against the architecture.

--- CombinePvals.py
+++ CombinePvals.py
@@ -364,63 +364,6 @@ def plot_top_snps(
     close()
 
 
-def make_ld_plot(
-    type_scores,
-    name,
-    bin_size=1e3,
-    max_dist=1e5,
-    new_figure=True,
-    outdir="analysis/results",
-    xmin=0,
-    xmax=1e4,
-):
-    """Plot correlation SNP-SNP correlation vs genomic distance
-
-    We don't yet have a great sense of what the linkage is in the populations
-    we're looking at. Flowers et al 2010 implies it should be small, but we
-    don't really know until we try it.
-    """
-    type_scores = pd.np.log10(type_scores.sort_index())
-    bins = defaultdict(list)
-    pairs_by_dist = defaultdict(list)
-    last_snp = ""
-    last_chr = ""
-    last_pos = -1
-    for snp in type_scores.index:
-        chr, pos = snp.split(":")
-        pos = int(pos)
-
-        if last_chr == chr and (pos - last_pos) <= max_dist:
-            dist_bin = int((pos - last_pos) // bin_size)
-            bins[dist_bin].append((type_scores[last_snp], type_scores[snp]))
-            pairs_by_dist[pos - last_pos].append(
-                (type_scores[last_snp], type_scores[snp])
-            )
-
-        last_snp = snp
-        last_chr = chr
-        last_pos = pos
-    corrs = pd.Series(index=bins.keys(), data=nan).sort_index()
-    counts = pd.Series(index=bins.keys(), data=-1).sort_index()
-
-    for bin, pvals in bins.items():
-        counts[bin] = len(pvals)
-        if counts[bin] > 2:
-            corrs[bin] = spearmanr(*zip(*pvals))[0]
-
-    if new_figure:
-        figure()
-    # corrs = corrs.dropna()
-    is_good = counts > 10
-    plot((corrs.index * bin_size)[is_good], corrs[is_good], label="Correlation")
-    xlim(xmin, min(xmax, max_dist))
-    ylim(-1, 1)
-    outfile = path.join(outdir, "{}_ld.png".format(name))
-    mpl.savefig(outfile)
-    # counts.plot()
-    return corrs, counts, pairs_by_dist
-
-
 if __name__ == "__main__":
     args = parse_args()
     outdir = path.dirname(args.output_prefix)
